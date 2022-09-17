700e6d7477abf122b0b406f9a3ca6541f6608f53
Loving <106512832+loving-cat@users.noreply.github.com>
2022-09-17 21:37:09 +0800

    Add files via upload

--- test_9_15.c
+++ test_9_15.c
@@ -95,11 +95,14 @@
 //offsetof
 //宏
 //用来计算结构体成员相对于起始位置的偏移量
+//#pragma pack(4)
 //struct S1
 //{
-//	char c1;
-//	int i;
-//	char c2;
+//	int c1;
+//	short i;
+//	int c2;
+//	char c3;
+//	char c4;
 //};
 //
 //struct S2
@@ -115,6 +118,9 @@
 //	printf("%d\n", offsetof(struct  S1,c1));
 //	printf("%d\n", offsetof(struct  S1, i));
 //	printf("%d\n", offsetof(struct  S1, c2));
+//	printf("%d\n", offsetof(struct  S1, c3));
+//	printf("%d\n", offsetof(struct  S1, c4));
+//
 //
 //	//printf("%d\n", sizeof(struct S1));
 //	//printf("%d\n", sizeof(struct S2));
@@ -122,45 +128,149 @@
 //}
 
 //设置默认对齐数
-#pragma pack(1)
+//#pragma pack(1)
+//
+//struct S1
+//{
+//	char c1;
+//	int i;
+//	char c2;
+//};
+//
+//#pragma pack()
+//
+//int main()
+//{
+//	printf("%d\n", sizeof(struct S1));
+//	return 0;
+//}
+//
+//struct S
+//{
+//	int data[1000];
+//	int num;
+//};
+//
+//void print1(struct S s)
+//{
+//	printf("%d %d %d %d\n", s.data[0], s.data[1], s.data[2], s.data[3]);
+//}
+//
+//void print2(const struct S* ps)
+//{
+//	//printf("%d %d %d %d\n", (*ps).data[0], (*ps).data[1], (*ps).data[2], (*ps).data[3]);
+//	printf("%d %d %d %d\n", ps->data[0], ps->data[1], ps->data[2], ps->data[3]);
+//
+//
+//}
+//int main()
+//{
+//	struct S ss = { {1,2,3,4,5},100 };
+//	print1(ss);
+//	print2(&ss);
+//	return 0;
+//}
 
-struct S1
-{
-	char c1;
-	int i;
-	char c2;
-};
+//struct A
+//{
+//	int a;
+//	short b;
+//	int c;
+//	char d;
+//};
+//struct B
+//{
+//	int a;
+//	short b;
+//	char c;
+//	int d;
+//};
+//
+//int main()
+//{
+//	printf("%d", sizeof(struct A));
+//	printf("%d", sizeof(struct B));
+//
+//	return 0;
+//}
 
-#pragma pack()
+//#pragma pack(4)/*编译选项，表示4字节对齐 平台：VS2013。语言：C语言*/
+//int main(int argc, char* argv[])
+//{
+//    struct tagTest1
+//    {
+//        short a;
+//        char d;
+//        long b;
+//        long c;
+//    };
+//    struct tagTest2
+//    {
+//        long b;
+//        short c;
+//        char d;
+//        long a;
+//    };
+//    struct tagTest3
+//    {
+//        short c;
+//        long b;
+//        char d;
+//        long a;
+//    };
+//   struct tagTest1 stT1;
+//    struct tagTest2 stT2;
+//    struct tagTest3 stT3;
+//    printf("%d %d %d", sizeof(stT1), sizeof(stT2), sizeof(stT3));
+//    return 0;
+//}
+//#pragma pack()
 
-int main()
-{
-	printf("%d\n", sizeof(struct S1));
-	return 0;
-}
+//typedef struct {
+//	int a;
+//	char b;
+//	short c;
+//	short d;
+//}AA_t;
+//int main()
+//{
+//	printf("%d\n", sizeof(AA_t));
+//	return 0;
+//}
 
-struct S
-{
-	int data[1000];
-	int num;
-};
+//struct A
+//{
+//	int a;
+//	short b;
+//	int c;
+//	char d;
+//};
+//int main()
+//{
+//	printf("%d\n", sizeof(struct A));
+//	return 0;
+//}
 
-void print1(struct S s)
-{
-	printf("%d %d %d %d\n", s.data[0], s.data[1], s.data[2], s.data[3]);
-}
+int main() {
+    int n = 0;
+    scanf("%d", &n);
+    //while (scanf("%d", &n) != EOF)
+    //{
+        int i = 0;
+        int j = 0;
+        for (i = 1; i <= n; i++)
+        {
+            for (j = 1; j <= n-i; j++)
+            {
+                printf("  ");
+            }
+            for (j = 1; j <= i; j++)
+            {
+                printf("* ");
+            }
+            printf("\n");
+        }
+    //}
 
-void print2(const struct S* ps)
-{
-	//printf("%d %d %d %d\n", (*ps).data[0], (*ps).data[1], (*ps).data[2], (*ps).data[3]);
-	printf("%d %d %d %d\n", ps->data[0], ps->data[1], ps->data[2], ps->data[3]);
-
-
-}
-int main()
-{
-	struct S ss = { {1,2,3,4,5},100 };
-	print1(ss);
-	print2(&ss);
-	return 0;
+    return 0;
 }
