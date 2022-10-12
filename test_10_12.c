1c1f59349745359f6a9b951f58c2a22b8f729c1a
Loving <106512832+loving-cat@users.noreply.github.com>
2022-10-12 21:44:08 +0800

    Add files via upload

---
+++ test_10_12.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include<stdio.h>
+//#define offsetof(id,ele) ((unsigned long)&((id*)0)->ele)
+//struct S
+//{
+//	char name;
+//	int age;
+//	float core;
+//};
+//int main()
+//{	
+//	printf("%d", offsetof(struct S , name));
+//	printf("%d", offsetof(struct S, age));
+//	printf("%d", offsetof(struct S, core));
+//
+//
+//	return 0;
+//}
