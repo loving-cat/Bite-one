96599b3e481f9f56bc811d72c04c30719f10310d
Xiao <106512832+loving-cat@users.noreply.github.com>
2022-10-23 19:45:39 +0800

    Add files via upload

---
+++ data10_21.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//#include<time.h>
+//int main()
+//{
+//	size_t begin = clock();
+//	size_t n = 0;
+//	for (size_t i = 0; i < 100; i++)
+//	{
+//		++n;
+//	}
+//	size_t end = clock();
+//	printf("%dºÁÃë\n", begin - end);
+//	return 0;
+//}
+
+void swap(int* x, int* y)
+{
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+void reverse(int* nums, int left, int right)
+{
+    while (left < right) {
+        swap(&nums[left], &nums[right]);
+        ++left;
+        --right;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+
+    k %= numsSize;
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}
