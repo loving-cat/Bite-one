0a9402ddcb16e6553acc136cb67a2038cd55f9f0
Xiao <106512832+loving-cat@users.noreply.github.com>
2022-11-05 18:35:33 +0800

    Add files via upload

--- 测试.c
+++ 测试.c
@@ -642,9 +642,60 @@ struct ListNode* removeElements(struct ListNode* head, int val) {
     }
     return reHead->next;
 }
+//合并两个有序链表
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
 
 
-while{cur->val != val}
-{
-    if()
+struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
+    if (list1 == NULL)
+        return list2;
+    if (list2 == NULL)
+        return list1;
+    struct ListNode* head;
+    struct ListNode* tail;
+    head = tail = NULL;
+
+    //取小的尾插
+    while (list1 && list2)
+    {
+        if (list1->val < list2->val)
+        {
+            if (tail == NULL)
+            {
+                head = tail = list1;
+            }
+            else
+            {
+                tail->next = list1;
+                tail = tail->next;
+            }
+
+            list1 = list1->next;
+        }
+        else
+        {
+            if (tail == NULL)
+            {
+                head = tail = list2;
+            }
+            else
+            {
+                tail->next = list2;
+                tail = tail->next;
+            }
+
+            list2 = list2->next;
+        }
+    }
+    if (list1)
+        tail->next = list1;
+    if (list2)
+        tail->next = list2;
+    return head;
 }
