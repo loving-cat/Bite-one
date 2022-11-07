58dd6f50e44d499df4c9a8f2ec7622e56e2f4ccf
Xiao <106512832+loving-cat@users.noreply.github.com>
2022-11-07 20:57:38 +0800

    Add files via upload

---
+++ Test2.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"SeqList.h"
+
+void TestSList1()
+{
+	SLTNode* n1 = BuySLTNode(1);
+	SLTNode* n2 = BuySLTNode(2);
+	SLTNode* n3 = BuySLTNode(3);
+	SLTNode* n4 = BuySLTNode(4);
+
+}
+
+int main()
+{
+	TestSList1();
+	return 0;
+}
