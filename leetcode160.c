3beb1e3ba56391a506b8dcd52b5013679d0f04e9
Xiao <106512832+loving-cat@users.noreply.github.com>
2022-11-09 17:41:11 +0800

    Add files via upload

---
+++ leetcode160.c
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* getIntersectionNode(struct ListNode* headA, struct ListNode* headB) {
+    struct ListNode* curA = headA, * curB = headB;
+    int lenA = 0; int lenB = 0;
+    //找尾
+    while (curA)
+    {
+        lenA++;
+        curA = curA->next;
+    }
+    while (curB)
+    {
+        lenB++;
+        curB = curB->next;
+    }
+
+    //尾节点不相等就不相交
+    if (curA != curB)
+        return NULL;
+
+    int gap = abs(lenA - lenB);
+    //假设认为A长B段
+    struct ListNode* longList = headA, * shortList = headB;
+    if (lenB > lenA)
+    {
+        longList = headB;
+        shortList = headA;
+    }
+
+    //先走差距步
+    while (gap--)
+    {
+        longList = longList->next;
+    }
+
+    //同时走，找交点
+    while (longList != shortList)
+    {
+        longList = longList->next;
+        shortList = shortList->next;
+    }
+
+    return longList;
+}
