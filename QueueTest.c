9dc7e0dd072e079ba78610b972d65787afb78ef8
Xiao <106512832+loving-cat@users.noreply.github.com>
2022-11-18 20:08:16 +0800

    Add files via upload

--- QueueTest.c
+++ QueueTest.c
@@ -3,7 +3,38 @@
 
 
 
+void TestQueue()
+{
+	Queue q1;
+	Queue q2;
+	QueueInit(&q1);
+	QueueInit(&q2);
+	QueueEmpty(&q1);
+	QueueEmpty(&q2);
+
+	QueueDestroy(&q1);
+	QueueDestroy(&q2);
+
+
+}
 int main()
 {
+	TestQueue();
 	return 0;
 }
+
+//leet
+//typedef struct {
+//	int* a;
+//	int front;
+//	int rear;
+//	int k;
+//} MyCircularQueue;
+//
+//
+//MyCircularQueue* myCircularQueueCreate(int k) {
+//	MyCircularQueue* obj = (MyCircularQueue*)malloc(sizeof(MyCircularQueue));
+//	obj->a = (int*)malloc(sizeof(int) * (k + 1));
+//	obj->front = obj->rear = 0;
+//	obj->k = k;
+//}
