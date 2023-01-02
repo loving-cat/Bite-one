6ca4b54781c2b84a012d8b8af7d66d2bb443e92e
Xiao <106512832+loving-cat@users.noreply.github.com>
2023-01-02 19:21:59 +0800

    Add files via upload

---
+++ SelectSort.c
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"sort.h"
+
+// —°‘Ò≈≈–Ú
+void SelectSort(int* a, int n)
+{
+	int begin = 0, end = n - 1;
+	while (begin < end)
+	{
+		int mini = begin, maxi = begin;
+		for (int i = begin + 1; i <= end; ++i)
+		{
+			if (a[i] < a[mini])
+			{
+				mini = i;
+			}
+
+			if (a[i] > a[maxi])
+			{
+				maxi = i;
+			}
+		}
+		Swap(&a[begin], &a[mini]);
+		if (maxi == begin)
+		{
+			maxi = mini;
+		}
+		Swap(&a[end], &a[maxi]);
+		++begin;
+		--end;
+	}
+}
